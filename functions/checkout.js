9bd2a9ad354e1627705eb0c64bb26204e3a50553
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-12-05 14:38:27 -0600

    Comment out unfinished function

--- functions/checkout.js
+++ functions/checkout.js
@@ -4,23 +4,28 @@ const { getFirestore } = require('firebase-admin/firestore');
 
 exports.placeOrder = functions.https.onCall(
     async (request, response) => {
-        const data = await new Promise((resolve, reject) => {
-            const db = getFirestore();
-            const order = db.collection('orders').doc();
-            const userId = request.auth.uid;
-            order.set({
-                for: db.doc(`users/${userId}`),
-            }).then(() => {
-                resolve({
-                    success: true
-                });
-            }).catch((error) => {
-                reject({
-                    success: false,
-                    error: error.message
-                });
-            });
-        });
-        return data;
+        // const data = await new Promise((resolve, reject) => {
+        //     const db = getFirestore();
+        //     const order = db.collection('orders').doc();
+        //     const userId = request.auth.uid;
+        //     order.set({
+        //         for: db.doc(`users/${userId}`),
+        //         items: [
+        //             {
+        //                 restaurant: db.doc(`restaurants/${request.data.restaurantId}`),
+        //             }
+        //         ]
+        //     }).then(() => {
+        //         resolve({
+        //             success: true
+        //         });
+        //     }).catch((error) => {
+        //         reject({
+        //             success: false,
+        //             error: error.message
+        //         });
+        //     });
+        // });
+        // return data;
     }
 );
