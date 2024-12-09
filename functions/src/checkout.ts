e11ef3e1b0b9d6ade99dd213d43bcd2709c9769e
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-12-08 23:03:12 -0600

    Make order placement functional

--- functions/src/checkout.ts
+++ functions/src/checkout.ts
@@ -1,36 +1,53 @@
 import * as functions from 'firebase-functions/v1';
-import { getFirestore } from 'firebase-admin/firestore';
+import { getFirestore, Timestamp } from 'firebase-admin/firestore';
+import { getRestaurantById } from './util';
+
+interface OrderItem {
+    restaurantId: string
+    quantity: number
+}
+
+interface OrderToPlace {
+    for: string // User ID
+    items: OrderItem[]
+}
 
 export const placeOrder = functions.https.onCall(
-    async (request, response) => {
-        const data = await new Promise((resolve, reject) => {
-            const db = getFirestore();
-            const orderItemsData = request.data.items;
-            const items = [];
-            for (const item of orderItemsData) {
-                const itemJson = {
-                    restaurant: db.doc(`restaurants/${item.restaurantId}`),
-                    quantity: item.quantity
-                }
-                items.push(itemJson)
+    async (data, context) => {
+        const responseData = await new Promise(async (resolve, reject) => {
+            const userId = context.auth?.uid;
+            if (!userId) {
+                reject(new Error("User is not authenticated."));
+                return;
             }
 
-            const order = db.collection('orders').doc();
-            const userId = request.auth.uid;
-            order.set({
+            const db = getFirestore();
+            const order = data as OrderToPlace;
+            const orderItemsData = order.items;
+
+            const items = [];
+            let totalPrice = 0;
+            for (const item of orderItemsData) {
+                const restaurant = await getRestaurantById(item.restaurantId);
+                const price = restaurant.bag_price * item.quantity;
+                const itemJson = {
+                    restaurant: db.doc(`restaurants/${item.restaurantId}`),
+                    quantity: item.quantity,
+                    price: price
+                }
+                items.push(itemJson);
+                totalPrice += price;
+            }
+
+            const orderDocument = db.collection('orders').doc();
+            orderDocument.set({
                 for: db.doc(`users/${userId}`),
-                items: items
-            }).then(() => {
-                resolve({
-                    success: true
-                });
-            }).catch((error) => {
-                reject({
-                    success: false,
-                    error: error.message
-                });
+                items: items,
+                totalPrice: totalPrice,
+                createdAt: Timestamp.fromMillis(Date.now())
             });
+            resolve({ success: true });
         });
-        return data;
+        return responseData;
     }
 );
